ca2f9b79899a6545c993fa978beec9d1b7d40bba
Franklin Assa <boykun@gmail.com>
2022-03-26 16:51:25 -0700

    x

--- frontend/src/FoodDetail.jsx
+++ frontend/src/FoodDetail.jsx
@@ -1,7 +1,6 @@
 import React from "react"
-import { Box, Card, CardMedia, Container, Typography, Button, Grid } from "@mui/material"
-import StarIcon from '@mui/icons-material/Star';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { Box, Card, CardMedia, Container, Typography } from "@mui/material"
+import { ArrowBackIosNew, Favorite, Star, LocationOn, Info, Map } from "@mui/icons-material"
 import styled from "@emotion/styled"
 
 const StyledTypography = styled(Typography)`
@@ -36,23 +35,22 @@ const FoodPrice = styled(StyledTypography)`
     font-weight: bold;
     margin: 5px;
 `
-
 const ButtonsContainer = styled(Container)`
     position: absolute;
     display: flex;
     justify-content: space-between;
     padding: 20px;
 `
-const FoodButtons = styled(ButtonsContainer)`
+const FoodButtonsContainer = styled(ButtonsContainer)`
     top: 0;
     left: 0;
 `
-const LinkButtons = styled(ButtonsContainer)`
+const LinkButtonsContainer = styled(ButtonsContainer)`
     bottom: 0;
     left: 0;
+    justify-content: space-around; 
 `
 const FoodDetailContainer = styled(Container)`
-    height: 50vh;
     padding: 0;
 `
 const FoodDescription = styled(StyledTypography)`
@@ -63,7 +61,7 @@ const FoodDescription = styled(StyledTypography)`
     font-size: 15px;
     font-weight: 500;
 `
-const NavButton = styled.div`
+const BackButton = styled.button`
     width: 40px;
     height: 40px;
     display: flex;
@@ -77,21 +75,43 @@ const NavButton = styled.div`
         transform: translate(2px, 2px);
         box-shadow: box-shadow: -10px -10px 0px 0px rgba(154, 159, 174, 0.2);
 }`
-
-const LinkButton = styled(Button)`
+const FavButton = styled(BackButton)`
+    color: #fff;    
+    background-color: #FFC520;
+    border-radius: 50%;
+`
+const LinkButton = styled.button`  
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: #FFC529;
-    border-radius: 10px;
+    color: #FFEFC3;
+    height: 50px;
+    padding: 20px;
+    border-radius: 40px;
+    border: none;
+    box-shadow: 8px 8px 25px 0px rgba(56, 70, 103, 0.15);
+    font-family: "Montserrat", sans-serif;
+    font-size: 15px;
+    font-weight: 600;
+    margin: 2px;
+    &:active {
+        transform: translate(2px, 2px);
+        box-shadow: box-shadow: -10px -10px 0px 0px rgba(154, 159, 174, 0.2);
 `
 
 const FoodDetail = () => {
   return (
-    <Box sx={{height:"100vh", padding: 1}}>
-        
-        <Card sx={{height:"40vh", borderRadius:"10px", backgroundColor:"red"}}>
-            <FoodButtons>
-                    <button>back</button>
-                    <button>like</button>
-            </FoodButtons>
+    <Box sx={{height:"98vh", padding: 1}}>
+        <Card 
+            sx={{
+                height:"40vh", 
+                borderRadius:"10px"
+                }}>
+            <FoodButtonsContainer>
+                    <BackButton><ArrowBackIosNew /></BackButton>
+                    <FavButton><Favorite /></FavButton>
+            </FoodButtonsContainer>
             <CardMedia
                 component="img"
                 image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-keto-pizza-073-1544039876.jpg"
@@ -99,45 +119,36 @@ const FoodDetail = () => {
                 height="100%"
             />
         </Card>
+
         <FoodDetailContainer>
             <FoodTitle variant="h4">Food Name</FoodTitle>
             <FoodBusiness variant="h5">
-                <LocationOnIcon style={{color:"FFC529"}}/> Restaurant
+                <LocationOn style={{color:"FFC529"}}/> Restaurant
             </FoodBusiness>
             <Container sx={{display:"flex", justifyContent:"space-between", padding:0}}>
+                <div>
                 <FoodRating variant="subtitle4">
-                    <StarIcon style={{fontSize:"20", color:"009933"}}/> 4.5
+                    <Star style={{fontSize:"20", color:"009933"}}/> 4.5
                 </FoodRating>
+                </div>
                 <FoodDistance>2mi</FoodDistance>
             </Container>
             <FoodPrice variant="h4">$10</FoodPrice>
-            
             <FoodDescription>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quasi id libero facilis facere. Voluptatibus cupiditate distinctio autem in, excepturi placeat culpa? Similique, laudantium aliquam? Ut, est? Iure quibusdam rerum inventore fugiat. Incidunt porro repellendus esse sint dolore deleniti eaque eveniet amet iure, similique tenetur quaerat. Vitae porro dolor dolorum.
             </FoodDescription>
-        
-        
         </FoodDetailContainer>
-        {/* <LinkButtons>
-            <div className="toyelp">yelp</div>
-            <div className="topmap">map</div>
-        </LinkButtons> */}
-        <Grid container 
-            direction="row" 
-            justifyContent="center" 
-            alignItems="center"
-        >
-                <Grid item xs>
-                    <Box textAlign={"center"}>
-                        <LinkButton variant="contained" startIcon={<StarIcon />}>YELP</LinkButton>
-                    </Box>
-                </Grid>
-                <Grid item xs>
-                    <Box textAlign={"center"}>
-                        <LinkButton variant="contained" startIcon={<StarIcon />}>MAPS</LinkButton>
-                    </Box>
-                </Grid>
-        </Grid>
+        
+        <LinkButtonsContainer>
+            <div>
+                <LinkButton><Info /> YELP</LinkButton>
+            </div>
+            <div>
+                <LinkButton><Map />MAPS</LinkButton>
+            </div>
+        </LinkButtonsContainer>
+
+        
         
     </Box>
   )
